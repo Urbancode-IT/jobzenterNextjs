311e8f9904232700819dfff5ee4e2b80f613c50c
SivaSankaraPandian3 <sivasankarapandian3@gmail.com>
2025-11-14 15:01:40 +0530

    mock interview

--- src/app/career-lab/mock interviews/page.js
+++
@@ -1,9 +0,0 @@
-//jobzenter/src/app/career-lab/bootcamps/page.js
-
-import Career from "@/components/career-labs/mock-interviews/Career";
-
-export default function Bootcamps() {
-  return <>
-  <Career />
-  </>;
-}
