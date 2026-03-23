395cc6afc22cd830288a1858ebaa6fcd9a324d01
Nallammal-3009 <nallammalcse2@gmail.com>
2026-03-23 13:50:15 +0530

    Home image

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ const AboutSection = () => {
     <div className="blob-dot dot-3"></div>
 
     <div className="blob-img-wrap">
-      <img src="/study-abroad/Place1.jpg" alt="JobZenter Student" className="blob-img" />
+      <img src="/Place1.jpg" alt="JobZenter Student" className="blob-img" />
     </div>
 
     <div className="blob-badge">
